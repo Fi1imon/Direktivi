5c4ebb4ee7e5c0bfce24ac469a2a60c8a714ed0f
Fi1imon <angelo4ekfilippof@gmail.com>
2022-02-20 16:26:43 +0300

    Создание структурной директивы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {StyleDirectives} from "./directives/style.directives";
 import { Style228Directive } from './style228.directive';
+import { IfnotDirective } from './directives/ifnot.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     StyleDirectives,
-    Style228Directive
+    Style228Directive,
+    IfnotDirective
   ],
   imports: [
     BrowserModule,
